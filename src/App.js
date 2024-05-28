51627bc22f619ad41cb855d2620454eee775d3f3
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-28 12:54:57 +0530

    Initialized Dashboard Panel

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { useState } from "react";
+import AuthenticationContext from "./context/AuthenticationContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
-
+  const [login,setLogin]=useState(false);
+  useEffect(() => {
+      const check = JSON.parse(localStorage.getItem("user"))?.email;
+      if (check) {
+        setLogin(true);
+      } else {
+        setLogin(false);
+      }
+    }, []);
   return (
     <div className={darkMode ? "app dark" : "app"}>
+      <AuthenticationContext.Provider value={{ login,setLogin }}>
       <BrowserRouter>
+      {
+        login?
         <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
-            </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              />
-            </Route>
-          </Route>
-        </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<List />} />
+        <Route path="/users/:userId" element={<Single />} />
+        <Route path="/users/new" element={<New inputs={userInputs} title="Add New User" />}/>
+        <Route path="/products" element={<List />} />
+        <Route path="/products/:productId" element={<Single />} />
+        <Route path="/products/new" element={<New inputs={productInputs} title="Add New Product" />}/>
+      </Routes>
+      :
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+      }
+       
       </BrowserRouter>
+      </AuthenticationContext.Provider>
     </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,81 @@
-import "./login.scss"
+import React, { useState, useContext } from "react";
+import stlyes from "./login.module.css";
+import AuthenticationContext from "../../context/AuthenticationContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
+function Login() {
+  const { setLogin } = useContext(AuthenticationContext);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const onChangeFunction = (event) => {
+    if (event.target.id === "InputEmail") {
+      setUser({ ...user, email: event.target.value });
+    } else {
+      setUser({ ...user, password: event.target.value });
+    }
+  };
+  const LoginFunction = async (event) => {
+    event.preventDefault();
+    try {
+      const status = await axios.post(`${process.env.REACT_APP_API}/login`, {
+        email: user.email,
+        password: user.password,
+      });
+      console.log(status);
+      if (status.status === 200) {
+        localStorage.setItem("user", JSON.stringify(status.data));
+        setLogin(true);
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className={stlyes.login}>
+      <form onSubmit={LoginFunction}>
+        <div className={`form-group ${stlyes.formDiv}`}>
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="InputEmail"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={user.email}
+            onChange={onChangeFunction}
+            required={true}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className={`form-group ${stlyes.formDiv}`}>
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="InputPassword1"
+            placeholder="Password"
+            value={user.password}
+            onChange={onChangeFunction}
+            required={true}
+          />
+        </div>
+        <button type="submit" className={`btn btn-primary ${stlyes.btn_color}`}>
+          Login
+        </button>
+      </form>
+      <div className={stlyes.loginFooter}>
+        <p>Powered by @Forelskets Software Solutions</p>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
