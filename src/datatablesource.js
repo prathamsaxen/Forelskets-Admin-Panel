3feeeb8e1654cfb1efb1b6d101ef744cda4579f3
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-28 15:43:52 +0530

    Enabled Data Tables

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -1,13 +1,13 @@
 export const userColumns = [
-  { field: "id", headerName: "ID", width: 70 },
+  { field: "id", headerName: "ID", width: 100 },
   {
-    field: "user",
-    headerName: "User",
+    field: "Name",
+    headerName: "Name",
     width: 230,
     renderCell: (params) => {
       return (
         <div className="cellWithImg">
-          <img className="cellImg" src={params.row.img} alt="avatar" />
+          {/* <img className="cellImg" src={params.row.img} alt="avatar" /> */}
           {params.row.username}
         </div>
       );
@@ -17,24 +17,6 @@ export const userColumns = [
     field: "email",
     headerName: "Email",
     width: 230,
-  },
-
-  {
-    field: "age",
-    headerName: "Age",
-    width: 100,
-  },
-  {
-    field: "status",
-    headerName: "Status",
-    width: 160,
-    renderCell: (params) => {
-      return (
-        <div className={`cellWithStatus ${params.row.status}`}>
-          {params.row.status}
-        </div>
-      );
-    },
   },
 ];
 
