1ea08a8fe89435c3a6524ba1760fb6b3268c3e93
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-29 13:14:17 +0530

    Gallery Code Setup

---
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function GalleryCard({data}) {
+  return (
+    <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="Card image cap"/>
+        <div className="card-body">
+            <h5 className="card-title">{data.name}</h5>
+            {/* <p className="card-text">{data.profile}</p> */}
+            <NavLink to={"/"} className="btn btn-primary">Delete</NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default GalleryCard;
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Gallery.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import GalleryCard from "../../components/GalleryCard/GalleryCard";
 
 function Gallery() {
   return (
@@ -9,10 +10,65 @@ function Gallery() {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        <h1>Gallery</h1>
+        <div className="galleryWrapper">
+          {demoData.map((item, index) => {
+            return <GalleryCard data={item} key={index} />;
+          })}
+        </div>
       </div>
     </div>
   );
 }
 
 export default Gallery;
+
+
+const demoData = [
+  {
+    name: "Pratham Saxena",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+  {
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },{
+    name: "Sujal Varshney",
+    profile: "Software Engineer",
+    image:
+      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+  },
+];
