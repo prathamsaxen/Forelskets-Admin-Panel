6151d27bf7e126e28ef11c5409695bd73f64fa73
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-29 17:43:27 +0530

    Added Delete User Functionality

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,19 +2,18 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 // import { userColumns } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { toast } from "react-toastify";
 
 export const userColumns = [
   { field: "_id", headerName: "ID", width: 250 },
-  {field: "name", headerName: "Name", width: 230,
+  {
+    field: "name",
+    headerName: "Name",
+    width: 230,
     renderCell: (params) => {
-      return (
-        <div className="cellWithImg">
-          {params.row.name}
-        </div>
-      );
+      return <div className="cellWithImg">{params.row.name}</div>;
     },
   },
   {
@@ -29,31 +28,33 @@ export const userColumns = [
   },
 ];
 
-
-
 const Datatable = () => {
   const [data, setData] = useState([]);
-
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-  useEffect(()=>{
-    const getUsers=async()=>{
-      try{
-        const  status=await axios.get(`${process.env.REACT_APP_API}api/user`);
-        if(status.status===200)
-          {
-            // console.log(status.data);
-            setData(status.data)
-          }
+  const deleteUser = async (id) => {
+    try {
+      const status = await axios.delete(
+        `${process.env.REACT_APP_API}api/user/${id}`
+      );
+      if (status.status === 200) {
+        toast.success("User removed successfully!");
       }
-      catch(err)
-      {
+    } catch (err) {
+      toast.error("Error deleting user");
+    }
+  };
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const status = await axios.get(`${process.env.REACT_APP_API}api/user`);
+        if (status.status === 200) {
+          setData(status.data);
+        }
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
     getUsers();
-  },[])
+  }, []);
 
   const actionColumn = [
     {
@@ -63,15 +64,14 @@ const Datatable = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
-              <div className="viewButton">View</div>
-            </Link>
-            <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            >
-              Delete
-            </div>
+            {JSON.parse(localStorage.getItem("user"))._id != params.row._id ? (
+              <div
+                className="deleteButton"
+                onClick={() => deleteUser(params.row._id)}
+              >
+                Delete
+              </div>
+            ) : null}
           </div>
         );
       },
@@ -91,7 +91,7 @@ const Datatable = () => {
         columns={userColumns.concat(actionColumn)}
         pageSize={10}
         rowsPerPageOptions={[10]}
-        getRowId={(row) => row._id} 
+        getRowId={(row) => row._id}
         // checkboxSelection
       />
     </div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <DarkModeContextProvider>
+        <ToastContainer/>
       <App />
     </DarkModeContextProvider>
   </React.StrictMode>,
