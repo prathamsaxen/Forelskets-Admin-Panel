185003bbe926dde3cdd28734d74de5181b1d76ee
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-30 16:59:34 +0530

    Setup Data And Mobile Number in Cards

--- src/components/ContactAccordion/ContactAccordion.jsx
+++ src/components/ContactAccordion/ContactAccordion.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
+import Collapse from "react-bootstrap/Collapse";
 import "./ContactAccordion.scss";
 
-function ContactAccordion({data}) {
-    const[visible,setVisible]=useState(false);
+function ContactAccordion({ data }) {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="card">
-      <div className="cardHeader">
-        <h2>
-          {data.name}
-        </h2>
-        <p>
-          {data.email}
-        </p>
+      <div className="cardHeader" onClick={() => setVisible(!visible)}>
+        <h2>{data.name}</h2>
+        <p>{data.email}</p>
       </div>
+      <Collapse in={visible}>
+        <div id="example-collapse-text">
+        <div className="upper">
+          <h2>{data.createdAt}</h2>
+          <p>{data.mobile}</p>
+        </div>
+          <p>{data.message}</p>
+        </div>
+      </Collapse>
     </div>
   );
 }
