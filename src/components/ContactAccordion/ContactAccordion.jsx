7319ddf0fd30f59e3a8c121ee6e57be5cde4f08b
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-31 20:30:00 +0530

    Contacts Card Fixed

--- src/components/ContactAccordion/ContactAccordion.jsx
+++ src/components/ContactAccordion/ContactAccordion.jsx
@@ -5,16 +5,16 @@ import "./ContactAccordion.scss";
 function ContactAccordion({ data }) {
   const [visible, setVisible] = useState(false);
   return (
-    <div className="cardAccordion">
-      <div className="cardHeaderAccordion" onClick={() => setVisible(!visible)}>
+    <div className="cardAccordion" onClick={() => setVisible(!visible)}>
+      <div className="cardHeaderAccordion" >
         <h2>{data.name}</h2>
         <p>{data.email}</p>
       </div>
       <Collapse in={visible}>
         <div>
         <div className="upperAccordion">
-          <h2>{data.createdAt}</h2>
           <p>{data.mobile}</p>
+          <p>{data.createdAt}</p>
         </div>
           <p className="paraAccordion">{data.message}</p>
         </div>
